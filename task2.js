ce14a19c1861184a0f500992de9b722150d7274b
behuamuh <sven1980@list.ru>
2018-06-05 23:01:26 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,20 +1,19 @@
 var a = process.argv[2]; 
-var bigInt = require("big-integer");
 function fibo (num) {
 	var n = Math.abs(num);
-	var f0 = bigInt(0);
-	var f1 = bigInt(1);
+	var f0 = 0;
+	var f1 = 1;
 	if (n==1 || n ==0)
   		return n;
  	var i;
  	var fi;
  	for (i=2; i <=n ; i++){ 
- 		fi = f0.add(f1);
+ 		fi = f0 + f1;
  		f0 = f1;
  		f1 = fi;
  	}
 	if (num < 0 && n % 2 == 0)
- 		fi = fi.multiply(-1);
+ 		fi = - fi;
  	return fi;
 	}
-console.log(fibo(a).toString());
+process.stdout.write(fibo(a).toString());
