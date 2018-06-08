7de55bde82473826fa2292f1c5933eec74fbb824
behuamuh <sven1980@list.ru>
2018-06-08 18:52:38 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -2,8 +2,12 @@ var str = process.argv[2];
 
 str = str.replace(/\W/g, '');
 var d = str.replace(/\D/g, '');
-var s = str.replace(/[aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ0123456789]/g,'');
-var g = str.replace(/[qwrtpsdfghjklzxcvbnmyQWRTPSDFGHJKLZXCVBNMYйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ0123456789]/g,'');
+var s = str.replace(/[aeiouyAEIOUYаеёиоуыэюяАЕЁИОУЫЭЮЯ0123456789]/g,'');
+var g = str.replace(/[qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNMйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ0123456789]/g,'');
 
-var res = g + " " + s + " " + d;
-process.stdout.write(res);
+if (g.length != 0)
+	g += " ";
+if (s.length != 0)
+	s+= " ";
+var res = g + s + d;
+process.stdout.write(res + '\n');
